d620e4e6c630dda140fca7c58a2eda07f8d3d8fa
dai-fuji <75709670+dai-fuji@users.noreply.github.com>
2026-02-03 16:29:48 +0000

    初期コミット

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="text-align: center; margin-top: 50px;">
+    <h1>Vue.js + S3 + GitHub Actions</h1>
+    <p>OIDCを利用して安全にデプロイされました！</p>
+  </div>
+</template>
+
+<style>
+body { font-family: sans-serif; background-color: #f0f2f5; }
+</style>
